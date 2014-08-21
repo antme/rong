37664772d0953a5d88e9b7051abe28917e5e527d
dylan <lieping.chen@yulor.com>
2014-08-21 12:57:56 +0800

    refine

--- api/app/response.php
+++ api/app/response.php
@@ -1,6 +1,6 @@
 <?php
 define ( 'CODE_PARAMETER_EMPTY', 1000 );
-define ( 'CODE_SUCCESS', 1 );
+define ( 'CODE_SUCCESS', 0 );
 
 header ( "Content-type:application/json" );
 function responseError($code, $msg) {
@@ -11,17 +11,13 @@ function responseError($code, $msg) {
 	echo json_encode ( $result );
 }
 function responseListData($data) {
-	$result = array ();
-	$result ['rtn_code'] = CODE_SUCCESS;
-	$result ['rtn_msg'] = "";
-	$result ['data'] = $data;
-	echo json_encode ( $result );
+	responseSingleData ( $data );
 }
-
 function responseSingleData($data) {
 	$result = array ();
 	$result ['rtn_code'] = CODE_SUCCESS;
-	$result ['rtn_msg'] = "";
+	$result ['rtn_msg'] = "success";
+	$result ['rtn_ext'] = "114.215.238.198|1.0";
 	$result ['data'] = $data;
 	echo json_encode ( $result );
 }
