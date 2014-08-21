995c244d1ad70866028eb29573273d75a219d715
dylan <lieping.chen@yulor.com>
2014-08-21 12:53:10 +0800

    update return

--- api/app/response.php
+++ api/app/response.php
@@ -5,23 +5,23 @@ define ( 'CODE_SUCCESS', 1 );
 header ( "Content-type:application/json" );
 function responseError($code, $msg) {
 	$result = array ();
-	$result ['code'] = $code;
-	$result ['msg'] = $msg;
+	$result ['rtn_code'] = $code;
+	$result ['rtn_msg'] = $msg;
 	
 	echo json_encode ( $result );
 }
 function responseListData($data) {
 	$result = array ();
-	$result ['code'] = CODE_SUCCESS;
-	$result ['msg'] = "";
-	$result ['rows'] = $data;
+	$result ['rtn_code'] = CODE_SUCCESS;
+	$result ['rtn_msg'] = "";
+	$result ['data'] = $data;
 	echo json_encode ( $result );
 }
 
 function responseSingleData($data) {
 	$result = array ();
-	$result ['code'] = CODE_SUCCESS;
-	$result ['msg'] = "";
+	$result ['rtn_code'] = CODE_SUCCESS;
+	$result ['rtn_msg'] = "";
 	$result ['data'] = $data;
 	echo json_encode ( $result );
 }
