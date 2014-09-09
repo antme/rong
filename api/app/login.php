87bd4b889bd50d4bfb222a00115c7cf5c688ea96
dylan <lieping.chen@yulor.com>
2014-09-09 20:02:40 +0800

    merge api

--- api/app/login.php
+++ api/app/login.php
@@ -8,46 +8,92 @@ C::app ()->init ();
 
 $mobile = $_REQUEST ["mobile"];
 $regcode = $_REQUEST ["regcode"];
-
-$valid = true;
 if (empty ( $mobile )) {
 	responseError ( CODE_PARAMETER_EMPTY, "手机号不能为空" );
 } else if (empty ( $regcode )) {
 	responseError ( CODE_PARAMETER_EMPTY, "验证码不能为空" );
 } else {
 	
-	$sql = "SELECT * FROM pre_common_member_profile where mobile='" . $mobile . "'";
-	$prifile = DB::fetch_first ( $sql );
-	if (empty ( $prifile ['uid'] )) {
-		echo $prifile ['uid'];
-		
-		responseError ( USER_REG_EXISTS, "此用户不存在" );
+	$sql = "SELECT * FROM pre_rong_reg_code where mobile='" . $mobile . "' and regcode='" . $regcode . "'";
+	$result = DB::fetch_first ( $sql );
+	
+	if (empty ( $result )) {
+		responseError ( REG_CODE_INVALID, "验证码不正确" );
 	} else {
 		
-		$sql = "SELECT * FROM pre_rong_reg_code where mobile='" . $mobile . "' and regcode='" . $regcode . "'";
-		$result = DB::fetch_first ( $sql );
+		$sql = "SELECT * FROM pre_common_member_profile where mobile='" . $mobile . "'";
+		$prifile = DB::fetch_first ( $sql );
 		
-		if (empty ( $result )) {
-			responseError ( REG_CODE_INVALID, "验证码不正确" );
+		$usql = "SELECT * FROM pre_common_member where username='" . $mobile . "'";
+		$user = DB::fetch_first ( $usql );
+		
+		if ($prifile ['uid'] || $user ['uid']) {
+			
+			$uid = $user ['uid'];
+			if ($prifile ['uid'] && empty ( $uid )) {
+				$uid = $prifile ['uid'];
+			}
+			login ( $mobile, $regcode, $uid );
 		} else {
-			$uid = $prifile ['uid'];
 			
-			$usql = "SELECT * from pre_common_member WHERE uid='" . $uid . "'";
-			$user = DB::fetch_first ( $usql );
-			
-			
-			$psql = "SELECT * from pre_common_member_profile WHERE uid='" . $uid . "'";
-			$puser = DB::fetch_first ( $psql );
-			
-			
-			$rdata = array (
-					'id' => $uid,
-					'username' => $user ['username'],
-					'nickname' => $puser ['field3'],
-					'token' => $uid 
-			);
-			responseSingleData ( $rdata );
+			reg ( $mobile, $regcode );
 		}
+	}
+}
+function login($mobile, $regcode, $uid) {
+	$usql = "SELECT * from pre_common_member WHERE uid='" . $uid . "'";
+	$user = DB::fetch_first ( $usql );
+	
+	$psql = "SELECT * from pre_common_member_profile WHERE uid='" . $uid . "'";
+	$puser = DB::fetch_first ( $psql );
+	
+	$rdata = array (
+			'id' => $uid,
+			'username' => $user ['username'],
+			'nickname' => $puser ['field3'],
+			'token' => $uid,
+			'action' => "login" 
+	);
+	responseSingleData ( $rdata );
+}
+function reg($mobile, $regCode) {
+	$password = $_REQUEST ["password"];
+	
+	if (empty ( $password )) {
+		$password = $mobile;
+	}
+	
+	$uid = uc_user_register ( $mobile, $password, $mobile . "@rong.com" );
+	
+	if ($uid == - 3) {
+		responseError ( USER_REG_EXISTS, "此用户已经存在" );
+	} else {
+		
+		$user = array ();
+		$user ['mobile'] = $mobile;
+		$user ['uid'] = $uid;
+		
+		DB::insert ( "common_member_profile", $user );
+		
+		uc_user_synlogin ( $uid );
+		
+		$user = array ();
+		$user ['email'] = $mobile . "@rong.com";
+		$user ['username'] = $mobile;
+		$user ['password'] = $password;
+		$user ['status'] = 0;
+		$user ['groupid'] = 10;
+		$user ['regdate'] = get_second ();
+		$user ['credits'] = 2;
+		$user ['timeoffset'] = "9999";
+		
+		DB::insert ( "common_member", $user );
+		$rdata = array (
+				'id' => $uid,
+				'token' => $uid,
+				'action' => 'reg' 
+		);
+		responseSingleData ( $rdata );
 	}
 }
 
--- api/app/reg.php
+++ api/app/reg.php
@@ -6,73 +6,7 @@ require_once '../../uc_client/client.php';
 require_once 'response.php';
 C::app ()->init ();
 
-$mobile = $_REQUEST ["mobile"];
-$regcode = $_REQUEST ["regcode"];
-$password = $_REQUEST ["password"];
 
-if (empty ( $password )) {
-	$password = $mobile;
-}
 
-/*
- * else if (empty ( $password )) { responseError ( CODE_PARAMETER_EMPTY, "密码不能为空" ); }
- */
-
-$valid = true;
-if (empty ( $mobile )) {
-	responseError ( CODE_PARAMETER_EMPTY, "手机号不能为空" );
-} else if (empty ( $regcode )) {
-	responseError ( CODE_PARAMETER_EMPTY, "验证码不能为空" );
-} else {
-	
-	$sql = "SELECT * FROM pre_common_member_profile where mobile='" . $mobile . "'";
-	$prifile = DB::fetch_all ( $sql );
-	
-	if (count ( $profile ) == 0) {
-		
-		$sql = "SELECT * FROM pre_rong_reg_code where mobile='" . $mobile . "' and regcode='" . $regcode . "'";
-		
-		$result = DB::fetch_all ( $sql );
-		
-		if (count ( $result ) == 1) {
-			$uid = uc_user_register ( $mobile, $password, $mobile . "@rong.com" );
-			
-			if ($uid == - 3) {
-				responseError ( USER_REG_EXISTS, "此用户已经存在" );
-			} else {
-				$rdata = array (
-						'id' => $uid,
-						'token' => $uid 
-				);
-				
-				$user = array ();
-				$user ['mobile'] = $mobile;
-				$user ['uid'] = $uid;
-				
-				DB::insert ( "common_member_profile", $user );
-				
-				uc_user_synlogin ( $uid );
-				
-				$user = array ();
-				$user ['email'] = $mobile . "@rong.com";
-				$user ['username'] = $mobile;
-				$user ['password'] = $password;
-				$user ['status'] = 0;
-				$user ['groupid'] = 10;
-				$user ['regdate'] = get_second ();
-				$user ['credits'] = 2;
-				$user ['timeoffset'] = "9999";
-				
-				DB::insert ( "common_member", $user );
-				
-				responseSingleData ( $rdata );
-			}
-		} else {
-			responseError ( REG_CODE_INVALID, "验证码不正确" );
-		}
-	} else {
-		responseError ( USER_REG_EXISTS, "此用户已经存在" );
-	}
-}
 
 ?>
