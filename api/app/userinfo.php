4ee3a3d46829a3adb2d14f6e7385de37e7369be1
dylan <lieping.chen@yulor.com>
2014-09-10 16:42:34 +0800

    for get user info

---
+++ api/app/userinfo.php
@@ -0,0 +1,36 @@
+<?php
+require '../../source/class/class_core.php';
+require 'response.php';
+C::app ()->init ();
+
+$token = $_REQUEST ["token"];
+
+$valid = true;
+if (empty ( $token )) {
+	responseError ( CODE_PARAMETER_EMPTY, "token不能为空" );
+} else {
+	$sql = "SELECT * FROM pre_common_member where uid='" . $token . "'";
+	$member = DB::fetch_first ( $sql );
+	
+	if (empty ( $member )) {
+		responseError ( USER_TOKEN_INVALID, "此token不存在或者已经失效" );
+	} else {
+		
+		$sql = "SELECT * FROM pre_common_member_profile where uid='" . $token . "'";
+		$prifile = DB::fetch_first ( $sql );
+		
+		$infodata = array (
+				"residedist" => $prifile ['residedist'],
+				"floor" => $prifile ['field1'],
+				"username" => $member ['username'],
+				"nickname" => $prifile ['field3'],
+				"gender" => $prifile ['gender'],
+				"occupation" => $prifile ['occupation'],
+				"room" => $prifile ['field2'] 
+		);
+		
+		responseSingleData ( $infodata );
+	}
+}
+
+?>
